3df966668dcb7479914603f4d8468e7a54f7ed51
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-01-25 18:52:53 +0100

    improving images

--- landing-page-webapp/quantencomputing-landing-page/components/QuantenComputingUiPreview/QuantenComputingUiPreview.tsx
+++ landing-page-webapp/quantencomputing-landing-page/components/QuantenComputingUiPreview/QuantenComputingUiPreview.tsx
@@ -5,15 +5,15 @@ export default function QuantenComputingUiPreview() {
     <div className={"md:flex justify-between space-y-5 md:space-y-0"}>
       <PreviewComponent
         title={"Learn Quantum Computing from the start"}
-        scr={"/images/getting-started-screen.svg"}
+        scr={"/images/blog-screen.png"}
       />
       <PreviewComponent
         title={"Run intuitive experiments"}
-        scr={"/images/add-screen.svg"}
+        scr={"/images/add-screen.png"}
       />
       <PreviewComponent
         title={"Get Results for your Experiments"}
-        scr={"/images/loading-screen.svg"}
+        scr={"/images/results-screen.png"}
       />
     </div>
   );
@@ -21,9 +21,13 @@ export default function QuantenComputingUiPreview() {
 
 function PreviewComponent(props: { title: string; scr: string }) {
   return (
-    <div className={"text-center w-full space-y-4"}>
-      <embed className={"w-full h-auto"} src={props.scr} />
-      <p className={"font-bold text-lg"}>{props.title}</p>
+    <div className={"text-center w-full relative"}>
+      <img className={"w-full h-auto"} src={props.scr} />
+      <p
+        className={"font-bold text-lg absolute bottom-0 inset-x-0 text-center"}
+      >
+        {props.title}
+      </p>
     </div>
   );
 }
